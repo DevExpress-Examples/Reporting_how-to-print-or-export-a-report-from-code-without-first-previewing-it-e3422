704bc7aa1615d8b68d58db3b7b9ff9606c4524c0
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 14:15:40 +0400

    Change example status to out of maintenance

--- CS/E3422.Web/DemoReportService.svc.cs
+++
@@ -1,7 +0,0 @@
-﻿using DevExpress.Data.Utils.ServiceModel;
-
-namespace E3422.Web {
-    [SilverlightFaultBehavior]
-    public class DemoReportService : DevExpress.XtraReports.Service.ReportService {
-    }
-}
--- VB/E3422.Web/DemoReportService.svc.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports DevExpress.Data.Utils.ServiceModel
-
-Namespace E3422.Web
-    <SilverlightFaultBehavior> _
-    Public Class DemoReportService
-        Inherits DevExpress.XtraReports.Service.ReportService
-
-    End Class
-End Namespace
